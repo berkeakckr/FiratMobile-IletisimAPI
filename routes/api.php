0140ac5ae02dcd925ad33b687ad8e03757b9c4ec
Berke Akçakır <94604291+berkeakckr@users.noreply.github.com>
2022-12-06 00:06:03 +0300

    laravel passport

--- routes/api.php
+++ routes/api.php
@@ -15,10 +15,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+Route::post('login',[\App\Http\Controllers\ApiController::class,'login']);
 
+Route::middleware('auth:api')->group(function()
+{
+
+    Route::get('/user',function()
+    {
+        return \Illuminate\Support\Facades\Auth::user();
+    });
+
+});
 
 //Route::get('test',[\App\Http\Controllers\TestController::class,'index']);
 //Route::post('post',[\App\Http\Controllers\TestController::class,'post']);
