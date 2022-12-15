b58d28484708970b941488ea9cd2b03a2efb40d7
Berke Akçakır <94604291+berkeakckr@users.noreply.github.com>
2022-12-15 16:58:28 +0300

    User Register Eklendi
    Mobil Testler için user create fonksiyonu eklendi ve birkaç migration değişikliği yapıldı.

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Laravel\Passport\Client;
 
 class ApiController extends Controller
 {
@@ -25,4 +27,52 @@ public function login(Request $request)
         'error'=>'Unauthorized'
     ],401);
 }
+
+  public function create(Request $request)
+    {
+        $valid = validator($request->only('email', 'name', 'password','type'), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:5',
+            'type' => 'required|boolean',
+        ]);
+
+        if ($valid->fails()) {
+            $jsonError=response()->json($valid->errors()->all(), 400);
+            return response()->json($jsonError);
+            //return \Response::json($jsonError);
+
+        }
+
+        $data = request()->only('email','name','password','type');
+
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+            'type' => $data['type']
+        ]);
+
+        $client = Client::where('password_client', 1)->first();
+
+        $request->request->add([
+            'grant_type'    => 'password',
+            'client_id'     => $client->id,
+            'client_secret' => $client->secret,
+            'username'      => $data['email'],
+            'password'      => $data['password'],
+            'scope'         => null,
+        ]);
+        $success['token']=$user->createToken("Login")->accessToken;
+        return response()->json([
+            'success'=>$success
+        ], 200);
+    }
+       /* // Fire off the internal request.
+        $token = Request::create(
+            'oauth/token',
+            'POST'
+        );
+        return \Route::dispatch($token);
+    }*/
 }
--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,9 @@ use Illuminate\Support\Facades\Route;
 //Route::middleware('auth:api')->get('/user', function (Request $request) {
 //    return $request->user();
 //});
+Route::post('register',[\App\Http\Controllers\ApiController::class,'create']);
 Route::post('login',[\App\Http\Controllers\ApiController::class,'login']);
+
 
 Route::middleware('auth:api')->group(function()
 {
