458fe827902ef1d160c5acdfff03fdba4a9de7d7
Berke Akçakır <94604291+berkeakckr@users.noreply.github.com>
2022-12-28 23:34:43 +0300

    Update MessageController.php

--- app/Http/Controllers/Api/MessageController.php
+++ app/Http/Controllers/Api/MessageController.php
@@ -37,86 +37,86 @@ class MessageController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
-    {
-      $user_type =UserConversation::where('user_id',$request->user_id)->where('conversation_id',$request->conversation_id)->first()->status;
-      $conversation= Conversation::where('id',$request->conversation_id)->first();
-      $user_conversation=UserConversation::where('conversation_id',$request->conversation_id->pluck('user_id'))->get();
+    { //saaa
+        $user_type =UserConversation::where('user_id',$request->user_id)->where('conversation_id',$request->conversation_id)->first()->status;
+        $conversation= Conversation::where('id',$request->conversation_id)->first();
+        $user_conversation=UserConversation::where('conversation_id',$request->conversation_id->pluck('user_id'))->get();
         //$user = Auth::user();
         $user= User::find($request->user_id);
-    if($user_type=='is_akademisyen')
-    {
-        //post metodu
-        $message = new Message();
-        $conversation = new Conversation();
-        $message->text = $request->text;
-        if($request->hasFile('file')){
-            $imageName=time().rand(1,1000).'.'.$request->file->getClientOriginalExtension();
-            $request->file->move(public_path('images'),$imageName);
-            $message->file='images/'.$imageName;
-        }
-        $message->user_id = $request->user_id;  //$user->id
-        $message->conversation_id = $request->conversation_id;
-        $conversation= Conversation::find($request->conversation_id);
-
-        $message->save();
-
-        $notification = Notification::create([
-            'message_id' => $message->id,
-            'user_id' => $user->id,
-            //'reached' => $data['email'],
-            // 'readed' => $data['type']
-        ]);
-
-        return response()->json(['message'=>'Mesaj Başarılı Bir Şekilde Oluşturuldu']);
-    }
-    if($user_type=='is_ogrenci'&& $conversation->type==0 && $user_conversation->status=='is_akademisyen'
-        && Auth::id()!=$user_conversation->user_id)
-    {
-        //post metodu
-        $message = new Message();
-        $conversation = new Conversation();
-        $message->text = $request->text;
-        if($request->hasFile('file')){
-            $imageName=time().rand(1,1000).'.'.$request->file->getClientOriginalExtension();
-            $request->file->move(public_path('images'),$imageName);
-            $message->file='images/'.$imageName;
-        }
-        $message->user_id = $request->user_id;  //$user->id
-        $message->conversation_id = $request->conversation_id;
-        $conversation= Conversation::find($request->conversation_id);
-
-        $message->save();
-
-        $notification = Notification::create([
-            'message_id' => $message->id,
-            'user_id' => $user->id,
-            //'reached' => $data['email'],
-            // 'readed' => $data['type']
-        ]);
-
-        return response()->json(['message'=>'Mesaj Başarılı Bir Şekilde Oluşturuldu']);
-    }
-    else{
-
-        return response()->json([
-            'error'=>'Bir öğrenci başka bir öğrenciye mesaj atamaz.'
-        ],401);
-    }
-
-
-
-
-       // $conversation->id = $request->conversation_id;
-      /*  if($conversation->type==0 && $conversation->id==1 && $user->type==1)
+        if($user_type=='is_akademisyen')
         {
+            //post metodu
+            $message = new Message();
+            $conversation = new Conversation();
+            $message->text = $request->text;
+            if($request->hasFile('file')){
+                $imageName=time().rand(1,1000).'.'.$request->file->getClientOriginalExtension();
+                $request->file->move(public_path('images'),$imageName);
+                $message->file='images/'.$imageName;
+            }
+            $message->user_id = $request->user_id;  //$user->id
+            $message->conversation_id = $request->conversation_id;
+            $conversation= Conversation::find($request->conversation_id);
+
             $message->save();
+
+            $notification = Notification::create([
+                'message_id' => $message->id,
+                'user_id' => $user->id,
+                //'reached' => $data['email'],
+                // 'readed' => $data['type']
+            ]);
+
             return response()->json(['message'=>'Mesaj Başarılı Bir Şekilde Oluşturuldu']);
         }
+        if($user_type=='is_ogrenci'&& $conversation->type==0 && $user_conversation->status=='is_akademisyen'
+            && Auth::id()!=$user_conversation->user_id)
+        {
+            //post metodu
+            $message = new Message();
+            $conversation = new Conversation();
+            $message->text = $request->text;
+            if($request->hasFile('file')){
+                $imageName=time().rand(1,1000).'.'.$request->file->getClientOriginalExtension();
+                $request->file->move(public_path('images'),$imageName);
+                $message->file='images/'.$imageName;
+            }
+            $message->user_id = $request->user_id;  //$user->id
+            $message->conversation_id = $request->conversation_id;
+            $conversation= Conversation::find($request->conversation_id);
 
+            $message->save();
+
+            $notification = Notification::create([
+                'message_id' => $message->id,
+                'user_id' => $user->id,
+                //'reached' => $data['email'],
+                // 'readed' => $data['type']
+            ]);
+
+            return response()->json(['message'=>'Mesaj Başarılı Bir Şekilde Oluşturuldu']);
+        }
+        else{
 
             return response()->json([
                 'error'=>'Bir öğrenci başka bir öğrenciye mesaj atamaz.'
-            ],401);*/
+            ],401);
+        }
+
+
+
+
+        // $conversation->id = $request->conversation_id;
+        /*  if($conversation->type==0 && $conversation->id==1 && $user->type==1)
+          {
+              $message->save();
+              return response()->json(['message'=>'Mesaj Başarılı Bir Şekilde Oluşturuldu']);
+          }
+
+
+              return response()->json([
+                  'error'=>'Bir öğrenci başka bir öğrenciye mesaj atamaz.'
+              ],401);*/
 
     }
 
