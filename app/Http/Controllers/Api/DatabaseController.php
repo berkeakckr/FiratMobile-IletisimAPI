4922db3f147a7fe840fc66aef03da5ddbbc39506
Tuncay FORMA <tuncayforma@hotmail.com>
2023-01-16 23:13:48 +0300

    conversation add

--- app/Http/Controllers/Api/DatabaseController.php
+++ app/Http/Controllers/Api/DatabaseController.php
@@ -8,73 +8,33 @@ use App\Models\Ders;
 use App\Models\UserConversation;
 use App\Models\UserDers;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DatabaseController extends Controller
 {
     public static function check()
     {
         $dersler = Ders::all();
-        $conversations = Conversation::all();
-        $user_dersler = UserDers::all();
-        $user_conversations = UserConversation::all();
-        if($conversations->isEmpty())
-        {
-            foreach($dersler as $ders)
-            {
+        foreach ($dersler as $ders){
+            if (!Conversation::where('ders_id',$ders->id)->first()){
                 $conversation = Conversation::create([
                     'title' => $ders->ders_adi,
                     'description' => $ders->ders_adi.' '."Açıklaması",
                     'type' => 0,
-                    'everyone_chat' => 0
+                    'everyone_chat' => 0,
+                    'ders_id' => $ders->id
                 ]);
             }
         }
-        else{
-            foreach($conversations as $conversation) {
-                foreach ($dersler as $ders) {
-                    if($ders->id==$conversation->id)
-                    {
-                        continue;
-                    }
-                    else{
-                        $conversation = Conversation::create([
-                            'title' => $ders->ders_adi,
-                            'description' => $ders->ders_adi . ' ' . "Açıklaması",
-                            'type' => 0,
-                            'everyone_chat' => 0
-                        ]);
-                    }
-                }
-            }
-        }
-        if($user_conversations->isEmpty())
-        {
-            foreach($user_dersler as $user_ders)
-            {
+        $user_dersler = UserDers::where('user_id',Auth::id())->get();
+        foreach ($user_dersler as $user_ders ){
+            if(!UserConversation::where('user_id',Auth::id())->where('conversation_id',Conversation::where('ders_id',$user_ders->id))->first()){
                 $user_conversation = UserConversation::create([
                     'user_id' => $user_ders->user_id,
                     'conversation_id' => $user_ders->id,
                     'send_message' => 0,
                     'status' => 0
                 ]);
-            }
-        }
-        else{
-            foreach($user_conversations as $user_conversation) {
-                foreach ($user_dersler as $user_ders) {
-                    if($user_ders->id==$user_conversation->id)
-                    {
-                        continue;
-                    }
-                    else{
-                        $user_conversation = UserConversation::create([
-                            'user_id' => $user_ders->user_id,
-                            'conversation_id' => $user_ders->id,
-                            'send_message' => 0,
-                            'status' => 0
-                        ]);
-                    }
-                }
             }
         }
     }
