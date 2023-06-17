ffb389ea4c77bbe2cb1474efe15c0c08814c5738
sidar-polat01 <sidar.polat01@gmail.com>
2023-06-17 22:36:58 +0300

    socket.io sunucu mesaj ayarlamaları

--- app/Socket/SocketIO.php
+++ app/Socket/SocketIO.php
@@ -51,5 +51,66 @@ class SocketIO
         }
     }
 
+    private function generateKey($length = 16)
+    {
+        $c = 0;
+        $tmp = '';
+        while ($c++ * 16 < $length) {
+            $tmp .= md5(mt_rand(), true);
+        }
+        return base64_encode(substr($tmp, 0, $length));
+    }
 
+    private function hybi10Encode($payload, $type = 'text', $masked = true)
+    {
+        $frameHead = array();
+        $payloadLength = strlen($payload);
+        switch ($type) {
+            case 'text':
+                $frameHead[0] = 129;
+                break;
+            case 'close':
+                $frameHead[0] = 136;
+                break;
+            case 'ping':
+                $frameHead[0] = 137;
+                break;
+            case 'pong':
+                $frameHead[0] = 138;
+                break;
+        }
+        if ($payloadLength > 65535) {
+            $payloadLengthBin = str_split(sprintf('%064b', $payloadLength), 8);
+            $frameHead[1] = ($masked === true) ? 255 : 127;
+            for ($i = 0; $i < 8; $i++) {
+                $frameHead[$i + 2] = bindec($payloadLengthBin[$i]);
+            }
+            if ($frameHead[2] > 127) {
+                $this->close(1004);
+                return false;
+            }
+        } elseif ($payloadLength > 125) {
+            $payloadLengthBin = str_split(sprintf('%016b', $payloadLength), 8);
+            $frameHead[1] = ($masked === true) ? 254 : 126;
+            $frameHead[2] = bindec($payloadLengthBin[0]);
+            $frameHead[3] = bindec($payloadLengthBin[1]);
+        } else {
+            $frameHead[1] = ($masked === true) ? $payloadLength + 128 : $payloadLength;
+        }
+        foreach (array_keys($frameHead) as $i) {
+            $frameHead[$i] = chr($frameHead[$i]);
+        }
+        if ($masked === true) {
+            $mask = array();
+            for ($i = 0; $i < 4; $i++) {
+                $mask[$i] = chr(rand(0, 255));
+            }
+            $frameHead = array_merge($frameHead, $mask);
+        }
+        $frame = implode('', $frameHead);
+        for ($i = 0; $i < $payloadLength; $i++) {
+            $frame .= ($masked === true) ? $payload[$i] ^ $mask[$i % 4] : $payload[$i];
+        }
+        return $frame;
+    }
 }
